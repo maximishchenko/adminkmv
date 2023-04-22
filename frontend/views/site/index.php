2f8fe7b75ef97239f72133f5c9009cd02c739ee2
Maxim Ishchenko <maxim.ishchenko@gmail.com>
2023-04-22 21:04:15 +0300

    maps

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -155,9 +155,8 @@ if (Yii::$app->configManager->getItemValue('loadingAnimation') == true) {
 </div>
 
 
-
-<script src="https://api-maps.yandex.ru/2.1/?apikey=<?= Yii::$app->configManager->getItemValue('mapApiKey'); ?>&lang=ru_RU" type="text/javascript"></script>
-<script src="js/map.js"></script>
+<!-- <script src="https://api-maps.yandex.ru/2.1/?apikey=<?php // echo Yii::$app->configManager->getItemValue('mapApiKey'); ?>&lang=ru_RU" type="text/javascript"></script> -->
+<!-- <script src="js/map.js"></script> -->
 
 <section 
     class="map__block slideanim"
@@ -169,3 +168,45 @@ if (Yii::$app->configManager->getItemValue('loadingAnimation') == true) {
 >
 
 </section>
+
+<script>
+setTimeout(function(){
+	var elem = document.createElement('script');
+	elem.type = 'text/javascript';
+	elem.src = '//api-maps.yandex.ru/2.1/?apikey=<?= Yii::$app->configManager->getItemValue('mapApiKey'); ?>&load=package.standard&lang=ru-RU&onload=getYaMap';
+	document.getElementsByTagName('body')[0].appendChild(elem);
+}, 5000);
+ 
+function getYaMap(){
+    var map_root = document.getElementById("map__block");
+    
+    var locations = map_root.getAttribute("data-coordinates").split(',');
+    var map_title = map_root.getAttribute("data-map-title");
+    var map_body = map_root.getAttribute("data-map-body");
+    var map_footer = map_root.getAttribute("data-map-footer");
+
+    var officeMap = new ymaps.Map("map__block", {
+        center: locations,
+        zoom: 16,
+        controls: [],
+    });
+    
+    var officePlaceMark = new ymaps.Placemark(locations, {
+            balloonContentHeader: map_title,
+            balloonContentBody: map_body,
+            balloonContentFooter: map_footer,
+            hintContent: map_title
+        }, {
+            overlayFactory: 'default#interactiveGraphics',
+            iconLayout: 'default#image',
+            iconImageHref: '/img/sprite.svg#logo',
+            iconImageSize: [60, 60],
+            iconImageOffset: [-30, -70],
+            iconCaption: map_title
+        }
+    );			 	
+        
+    officeMap.geoObjects.add( officePlaceMark );
+    officeMap.behaviors.disable('scrollZoom');
+}
+</script>
